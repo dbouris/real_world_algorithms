893084c23898f57042c835eb38ce1aff0a842fc6
dbouris <bourisd2@gmail.com>
2022-09-28 19:09:14 +0300

    add comments in the beckett_gray code

--- Beckett-Grey/beckett_gray.py
+++ Beckett-Grey/beckett_gray.py
@@ -1,14 +1,15 @@
+import time
 import argparse
 
 # initialize and parameterize the parser
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-a', action='store_true' ,default="")
-group.add_argument('-b', action='store_true',default="")
+group.add_argument('-a', action='store_true', default="")
+group.add_argument('-b', action='store_true', default="")
 group.add_argument('-u', action='store_true', default="")
 group.add_argument('-c', action='store_true', default="")
-group.add_argument('-p', action='store_true', default= "")
-group.set_defaults(type = "a")
+group.add_argument('-p', action='store_true', default="")
+group.set_defaults(type="a")
 
 parser.add_argument('-r', action='store_true')
 parser.add_argument('-f', action='store_true')
@@ -20,55 +21,59 @@ if not any((args.a, args.b, args.u, args.c, args.p)):
     args.a = True
 
 
-
-
-
-visited ={}
+visited = {}
 l = []
+
+
 def generateAllBinaryStrings(n, arr, i):
- 
+
     if i == n:
         printTheArray(arr, n)
         return
-     
+
     arr[i] = 0
     generateAllBinaryStrings(n, arr, i + 1)
- 
+
     arr[i] = 1
     generateAllBinaryStrings(n, arr, i + 1)
 
+
 def printTheArray(arr, n):
-    l=[]
+    l = []
     s = ""
     for i in range(0, n):
         l.append(str(arr[i]))
         s = s + str(arr[i])
-    visited[s]= 0 
+    visited[s] = 0
     all.append(l)
 
 # function to find the cycle in the code
+
+
 def find_cyrcle(x):
     l = list(x)
-    for i in range(0,len(x)):
-        if l[i]=="1":
-            l[i]="0"
+    for i in range(0, len(x)):
+        if l[i] == "1":
+            l[i] = "0"
             x2 = "".join(l)
-            if x2== "0"* n:
+            if x2 == "0" * n:
                 return n-1-i
         l = list(x)
     return -1
 
 # function to get the full binary representation of the code
-def full_binary(n,delta,par):
+
+
+def full_binary(n, delta, par):
     x = "0"*n
     final = []
     final.append(x)
     for i in delta:
         l = list(x)
-        if l[n-1-int(i)]=="0":
-            l[n-1-int(i)]="1"
+        if l[n-1-int(i)] == "0":
+            l[n-1-int(i)] = "1"
         else:
-            l[n-1-int(i)]="0"
+            l[n-1-int(i)] = "0"
         x = "".join(l)
         final.append(x)
     if par != -1:
@@ -76,89 +81,93 @@ def full_binary(n,delta,par):
     return final
 
 # function to prin the code in matrix form
+
+
 def print_in_matrix(full):
-    for i in range(0,n):
+    for i in range(0, n):
         for x in full:
-            print(x[n-i-1],end = " ")
+            print(x[n-i-1], end=" ")
         print()
 
-def flip(x,i,k):
-    
-    l = list(x)
-    if l[n-1-i]=="0":
-        l[n-1-i]="1"
-    else:
-        l[n-1-i]="0"
-    x = "".join(l)
-    return x 
+# the function changes the value of bit i in the code x
 
-# check if the code is Canonic
+
+def flip(x, i, k):
+    l = list(x)
+    if l[n-1-i] == "0":
+        l[n-1-i] = "1"
+    else:
+        l[n-1-i] = "0"
+    x = "".join(l)
+    return x
+
+# check if the code is Canonical
 def check_canonical(code):
     d = {}
-    for i in range(0,n):
-        d[i]= 0
+    for i in range(0, n):
+        d[i] = 0
     for c in code:
         if d[int(c)] == 0:
-            for k in range(0,int(c)):
+            for k in range(0, int(c)):
                 if d[k] == 0:
                     return 0
             d[int(c)] = 1
 
     return 1
-    
-    
 
 
-def flip2(x,i,k,queue):
-    po= -1
+def flip2(x, i, k, queue):
+    po = -1
     l = list(x)
-    if l[n-1-i]=="0":
-        l[n-1-i]="1"
+    if l[n-1-i] == "0":
+        l[n-1-i] = "1"
         queue.append(n-1-i)
-        po = 1 
+        po = 1
     else:
-        if len(queue)==0:
-            l[n-1-i]="0"
+        if len(queue) == 0:
+            l[n-1-i] = "0"
         else:
             fi = queue[0]
             if n-1-i == fi:
-                l[n-1-i]="0"
+                l[n-1-i] = "0"
                 queue.pop(0)
-                po = 0 
+                po = 0
             else:
                 return x, -1, po
     x = "".join(l)
     return x, 1, po
 
 
-all_codes= []
+# The algorithm builds Gray codes one at a time
+all_codes = []
 gc = []
 
-def GC_DFS(d,x,max_cord,n,gc):
+
+def GC_DFS(d, x, max_cord, n, gc):
 
     if d == (2**n):
         p = gc[:]
         r = find_cyrcle(x)
         p.append(r)
         all_codes.append(p)
-        return 
-    
-    
-    for i in range(0,min(n,max_cord+1)):
-        x = flip(x,i,1)
+        return
+
+    for i in range(0, min(n, max_cord+1)):
+        x = flip(x, i, 1)
         if not visited[x]:
-            visited[x]= 1
+            visited[x] = 1
             gc.append(i)
-            GC_DFS(d+1,x,max(i+1,max_cord),n,gc)
-            visited[x]= 0
+            GC_DFS(d+1, x, max(i+1, max_cord), n, gc)
+            visited[x] = 0
             gc.pop()
-        x = flip(x,i,2)
+        x = flip(x, i, 2)
 
 
-
-all_codes= []
+all_codes = []
 gc = []
-def GC_BECKETT_DFS(d,x,max_cord,n,gc,queue):
+
+
+def GC_BECKETT_DFS(d, x, max_cord, n, gc, queue):
 
     if d == (2**n):
 
@@ -166,224 +175,209 @@ def GC_BECKETT_DFS(d,x,max_cord,n,gc,queue):
         r = find_cyrcle(x)
         p.append(r)
         all_codes.append(p)
-        return 
-    
-    
-    for i in range(0,min(n,max_cord+1)):
-        (x, r,ar)  = flip2(x,i,1,queue)
+        return
+
+    for i in range(0, min(n, max_cord+1)):
+        (x, r, ar) = flip2(x, i, 1, queue)
         if visited[x]:
             if ar == 1:
                 queue.pop()
-            elif ar ==0:
-                queue.insert(0,n-1-i)
-                
-        if not visited[x] and r != -1: 
-            visited[x]= 1
+            elif ar == 0:
+                queue.insert(0, n-1-i)
+
+        if not visited[x] and r != -1:
+            visited[x] = 1
             gc.append(i)
-            GC_BECKETT_DFS(d+1,x,max(i+1,max_cord),n,gc,queue)
-            if ar ==1:
+            GC_BECKETT_DFS(d+1, x, max(i+1, max_cord), n, gc, queue)
+            if ar == 1:
                 queue.pop()
             elif ar == 0:
-                queue.insert(0,n-1-i)
-            
-            visited[x]= 0
+                queue.insert(0, n-1-i)
+
+            visited[x] = 0
             gc.pop()
         if ar != -1:
-            x = flip(x,i,2)
+            x = flip(x, i, 2)
 
-
-
-
-
-import time
-
-
+# if we need to find all the codes or find the Gray paths
+# or find the cyclical codes
 if args.a or args.p or args.c:
     all = []
-    
+
     all_codes = []
-    n=args.number_of_bits
+    n = args.number_of_bits
     arr = [None] * n
     generateAllBinaryStrings(n, arr, 0)
     x = "0"*n
-    visited[x] = 1 
+    visited[x] = 1
     queue = []
-    GC_DFS(1,x,0,n,gc)
+    GC_DFS(1, x, 0, n, gc)
 
-    
+    # find all the codes (cycles and paths)
     if args.a:
         for i in all_codes:
-            if i[-1]== -1:
+            if i[-1] == -1:
                 i.remove(-1)
-                print("P", end = " ")
+                print("P", end=" ")
                 for k in i:
-                    print(k, end= "")
+                    print(k, end="")
                 print()
                 if args.f:
-                    print("P", end = " ")
-                    m = full_binary(n,i,-1)
+                    print("P", end=" ")
+                    m = full_binary(n, i, -1)
                     for i in m:
                         for k in i:
-                            print(k, end = "")
-                        print(" ", end ="")
+                            print(k, end="")
+                        print(" ", end="")
                     print()
 
                 if args.m:
-                    m = full_binary(n,i,-1)
+                    m = full_binary(n, i, -1)
                     print_in_matrix(m)
             else:
-                print("C", end = " ")
+                print("C", end=" ")
                 for k in i:
-                    print(k, end= "")
+                    print(k, end="")
                 print()
                 if args.m:
-                    m = full_binary(n,i,1)
+                    m = full_binary(n, i, 1)
                     print_in_matrix(m)
                 if args.f:
-                    print("C", end = " ")
-                    m = full_binary(n,i,1)
+                    print("C", end=" ")
+                    m = full_binary(n, i, 1)
                     for i in m:
                         for k in i:
-                            print(k, end = "")
-                        print(" ", end ="")
+                            print(k, end="")
+                        print(" ", end="")
                     print()
     
+    # find cyclical codes
     if args.c:
         for i in all_codes:
             if i[-1] != -1:
-                print("C", end = " ")
+                print("C", end=" ")
                 for k in i:
-                    print(k, end= "")
+                    print(k, end="")
                 print()
                 if args.m:
-                    m = full_binary(n,i,1)
+                    m = full_binary(n, i, 1)
                     print_in_matrix(m)
-    
+    # find Gray paths
     if args.p:
         for i in all_codes:
-            if i[-1]== -1:
+            if i[-1] == -1:
                 i.remove(-1)
-                print("P", end = " ")
+                print("P", end=" ")
                 for k in i:
-                    print(k, end= "")
+                    print(k, end="")
                 print()
                 if args.f:
-                    print("P", end = " ")
-                    m = full_binary(n,i,-1)
+                    print("P", end=" ")
+                    m = full_binary(n, i, -1)
                     for i in m:
                         for k in i:
-                            print(k, end = "")
-                        print(" ", end ="")
+                            print(k, end="")
+                        print(" ", end="")
                     print()
 
                 if args.m:
-                    m = full_binary(n,i,-1)
+                    m = full_binary(n, i, -1)
                     print_in_matrix(m)
-
-elif args.b or args.u :
+# if we need to find Beckett codes or find the Gray paths (not the cycles)
+elif args.b or args.u:
     all = []
-    
+
     all_codes = []
-    n=args.number_of_bits
+    n = args.number_of_bits
     arr = [None] * n
     generateAllBinaryStrings(n, arr, 0)
-    x = "0" * n 
-    visited[x] = 1 
+    x = "0" * n
+    visited[x] = 1
     queue = []
-    GC_BECKETT_DFS(1,x,0,n,gc,queue)
+    GC_BECKETT_DFS(1, x, 0, n, gc, queue)
 
+    # if there are no becket-gray cycles found, show the unfinished ones
 
-    #if there are no becket-gray cycles found, show the unfinished ones
-
-    u = 0 
+    u = 0
     for i in all_codes:
-        if i[-1]== -1:
-            u = u +1
-    
-    b = len(all_codes)- u
+        if i[-1] == -1:
+            u = u + 1
 
+    b = len(all_codes) - u
 
     becket_c = []
     becket_u = []
 
-
+    # find the Beckett-Gray codes
     if args.b:
         for i in all_codes:
-            if i[-1]== -1:
+            if i[-1] == -1:
                 i.remove(-1)
                 becket_u.append(i)
-                if b == 0 :    
-                    print("U", end = " ")
+                if b == 0:
+                    print("U", end=" ")
                     for k in i:
-                        print(k, end= "")
+                        print(k, end="")
                     print()
                     if args.f:
-                        print("U", end = " ")
-                        m = full_binary(n,i,-1)
+                        print("U", end=" ")
+                        m = full_binary(n, i, -1)
                         for i in m:
                             for k in i:
-                                print(k, end = "")
-                            print(" ", end ="")
+                                print(k, end="")
+                            print(" ", end="")
                         print()
                     if args.m:
-                        m = full_binary(n,i,-1)
+                        m = full_binary(n, i, -1)
                         print_in_matrix(m)
             else:
                 becket_c.append(i)
-                print("B", end = " ")
+                print("B", end=" ")
                 for k in i:
-                    print(k, end= "")
+                    print(k, end="")
                 print()
                 if args.f:
-                    print("Β", end = " ")
-                    m = full_binary(n,i,1)
+                    print("Β", end=" ")
+                    m = full_binary(n, i, 1)
                     for i in m:
                         for k in i:
-                            print(k, end = "")
-                        print(" ", end ="")
+                            print(k, end="")
+                        print(" ", end="")
                     print()
                 if args.m:
-                    m = full_binary(n,i,1)
+                    m = full_binary(n, i, 1)
                     print_in_matrix(m)
-            
-            
-
+    # find the Beckett-Gray paths (not the cycles)
     if args.u:
         for i in all_codes:
             if i[-1] == -1:
                 i.remove(-1)
-                print("U", end = " ")
+                print("U", end=" ")
                 for k in i:
-                    print(k, end= "")
+                    print(k, end="")
                 print("")
                 if args.f:
-                    print("U", end = " ")
-                    m = full_binary(n,i,-1)
+                    print("U", end=" ")
+                    m = full_binary(n, i, -1)
                     for i in m:
                         for k in i:
-                            print(k, end = "")
-                        print(" ", end ="")
+                            print(k, end="")
+                        print(" ", end="")
                     print()
                 if args.m:
-                    m = full_binary(n,i,-1)
+                    m = full_binary(n, i, -1)
                     print_in_matrix(m)
 
-
-
-
-
-
-
+# find the reverse isomorphic codes
 if args.r:
-    
-    if b >0:
+
+    if b > 0:
         all_codes = becket_c
     else:
         all_codes = becket_u
-    
 
-    #get all the codes in a string form
-    all_codes_new  = []         
+    # get all the codes in a string form
+    all_codes_new = []
     for i in all_codes:
         if i[-1] == "-1":
             i.pop()
@@ -391,24 +385,21 @@ if args.r:
         for k in i:
             code = code + str(k)
         all_codes_new.append(code)
-    
 
-
-
-    import itertools 
+    import itertools
     s = ""
-    for k in range(0,n):
+    for k in range(0, n):
         s = s + str(k)
-    l = list(itertools.permutations(s,n))
+    l = list(itertools.permutations(s, n))
     l.pop(0)
-    found= []
+    found = []
 
     li = []
     for i in all_codes_new:
         for permutation in l:
             code = ""
             for k in i:
-                    code = code + permutation[int(k)]
+                code = code + permutation[int(k)]
             li.append(code)
 
     f = []
@@ -418,22 +409,21 @@ if args.r:
         rev = "".join(lo)
         if check_canonical(rev):
             f.append(rev)
-  
 
     matches = {}
     for i in all_codes_new:
-         matches[i]= []
-    
-    #ADDITIONS add check for when the same string is produced from a code
-    #AND do not print the code that has no isomorphic codes produced from it
+        matches[i] = []
+
+    # ADDITIONS add check for when the same string is produced from a code
+    # AND do not print the code that has no isomorphic codes produced from it
     for code_searching in all_codes_new:
-        #find all the possible metathesis
-        #reverse them 
-        #check if they are canonical
+        # find all the possible metathesis
+        # reverse them
+        # check if they are canonical
 
         new_isomorphic = []
         reversed = []
-        
+
         for permutation in l:
             code = ""
             for k in code_searching:
@@ -444,7 +434,7 @@ if args.r:
             lo.reverse()
             rev = "".join(lo)
             if check_canonical(rev):
-                if rev in all_codes_new and  rev not in matches[code_searching] and code_searching not in matches[rev]:
+                if rev in all_codes_new and rev not in matches[code_searching] and code_searching not in matches[rev]:
                     if rev != code_searching:
                         matches[code_searching].append(rev)
                         print(code_searching, "<=>", rev)
